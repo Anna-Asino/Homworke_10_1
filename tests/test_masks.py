06d2e7ed597133fdf61b168cba9179f6a740777b
Anna Liseva <anuta.030481@gmail.com>
2024-08-06 18:44:00 +0700

    add test_masks

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -1,12 +1,16 @@
 import pytest
+
 from src.masks import get_mask_card_number, get_mask_account
 
 
-@pytest.mark.parametrize('card_numb','card_number_sim',[
-    ('1234567891234564', '1234 56** **** **64'),
-    ('1234567891234789', '1234 56** **** **89'),
-    ('1234567891234001', '1234 56** **** **01')
+@pytest.mark.parametrize(
+    'card_numb', 'expected',
+    [
+        ('1234567891234564', '1234 56** **** **64'),
+        ('1234567891234789', '1234 56** **** **89'),
+        ('1234567891234001', '1234 56** **** **01')
 
-])
-def test_get_mask_card_number(card_numb,card_number_sim):
-    assert test_get_mask_card_number(card_numb) == card_number_sim
+    ]
+)
+def test_get_mask_card_number(card_numb, expected):
+    assert test_get_mask_card_number(card_numb) == expected
