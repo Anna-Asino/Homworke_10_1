a3d1e585792857c4c38f9037fcb622defe28a7c7
Anna Liseva <anuta.030481@gmail.com>
2024-08-14 18:41:05 +0700

    add test_decorators

---
+++ tests/test_decorators.py
@@ -0,0 +1,65 @@
+import pytest
+import tempfile
+from src.decorators import my_function, log
+
+
+def test_log_good(capsys):
+    """Тестирует выполнение декорированной функции"""
+
+    @log()
+    def func(x, y):
+        return x + y
+
+    result = func(1, 2)
+
+    assert result == 3
+
+
+def test_log_good_file_log(capsys):
+    """Тестирует запись в файл после успешного выполнения"""
+
+    with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+        log_file_path = tmp_file.name
+
+    @log(filename=log_file_path)
+    def func(x, y):
+        return x + y
+
+    func(1, 2)
+
+    with open(log_file_path, 'r', encoding='utf-8') as file:
+        logs = file.read()
+
+    assert "my_function ok" in logs
+
+
+# def test_log_exception(capsys):
+#     """Тестирует вывод после ошибки в консоль"""
+#
+#     @log()
+#     def func(x, y):
+#         return x + y
+#
+#     func(1, "2")
+#
+#     captured = capsys.readouterr()
+#
+#     assert "my_function error" in captured.out
+#
+#
+# def test_log_exception_file_log(capsys):
+#     """Тестирует запись в файл после ошибки"""
+#
+#     with tempfile.NamedTemporaryFile(delete=False) as tmp_file:
+#         log_file_path = tmp_file.name
+#
+#     @log(filename=log_file_path)
+#     def func(x, y):
+#         return x + y
+#
+#     func(1, "2")
+#
+#     with open(log_file_path, 'r', encoding='utf-8') as file:
+#         logs = file.read()
+#
+#     assert "my_function error" in logs
